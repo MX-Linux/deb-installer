df03817d12b180b4857567bd41123d7f46c3c88b
Adrian <adrian@mxlinux.org>
2022-12-25 17:36:41 -0500

    New build

--- translations/deb-installer_ja.ts
+++ translations/deb-installer_ja.ts
@@ -4,33 +4,33 @@
 <context>
     <name>Installer</name>
     <message>
-        <location filename="../installer.cpp" line="84"/>
+        <location filename="../installer.cpp" line="85"/>
         <source>Remove</source>
         <translation>削除</translation>
     </message>
     <message>
-        <location filename="../installer.cpp" line="86"/>
-        <location filename="../installer.cpp" line="118"/>
+        <location filename="../installer.cpp" line="87"/>
+        <location filename="../installer.cpp" line="117"/>
         <source>Install</source>
         <translation>インストール</translation>
     </message>
     <message>
-        <location filename="../installer.cpp" line="89"/>
+        <location filename="../installer.cpp" line="90"/>
         <source>The following packages will be installed. Click &apos;Show Details...&apos; for information about the packages.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../installer.cpp" line="97"/>
+        <location filename="../installer.cpp" line="98"/>
         <source>File: %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../installer.cpp" line="116"/>
+        <location filename="../installer.cpp" line="115"/>
         <source>Will install the following:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../installer.cpp" line="132"/>
+        <location filename="../installer.cpp" line="131"/>
         <source>Press any key to close</source>
         <translation>任意のキーを押せば閉じます</translation>
     </message>
@@ -38,60 +38,60 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="48"/>
+        <location filename="../main.cpp" line="46"/>
         <source>Program for installing Debian binary packages (deb files)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="51"/>
+        <location filename="../main.cpp" line="49"/>
         <source>files...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="51"/>
+        <location filename="../main.cpp" line="49"/>
         <source>Name of .deb files to install</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="52"/>
+        <location filename="../main.cpp" line="50"/>
         <source>[file...]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="72"/>
+        <location filename="../main.cpp" line="69"/>
         <source>Select .deb files to install</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="73"/>
+        <location filename="../main.cpp" line="70"/>
         <source>Deb Files (*.deb)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="78"/>
-        <location filename="../main.cpp" line="84"/>
-        <location filename="../main.cpp" line="88"/>
-        <location filename="../main.cpp" line="95"/>
+        <location filename="../main.cpp" line="75"/>
+        <location filename="../main.cpp" line="82"/>
+        <location filename="../main.cpp" line="86"/>
+        <location filename="../main.cpp" line="93"/>
         <source>Error</source>
         <translation>エラー</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="78"/>
+        <location filename="../main.cpp" line="75"/>
         <source>No .deb files were provided.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="84"/>
+        <location filename="../main.cpp" line="82"/>
         <source>File %1 not found</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="89"/>
+        <location filename="../main.cpp" line="87"/>
         <source>File %1 is not a .deb file.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="95"/>
+        <location filename="../main.cpp" line="93"/>
         <source>You must run this program as normal user.</source>
         <translation>このプログラムは標準ユーザーで実行して下さい。</translation>
     </message>
